fbaf3441d8cfa6261158de90077b43f6b44ba733
AlexandreAdam <adam.alexandre01123@gmail.com>
2023-08-21 17:17:34 -0400

    Added job scripts, architectures and updated training script

---
+++ jobs/train_unconditional_ncsnpp_i.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --tasks=1
+#SBATCH --cpus-per-task=1 # maximum cpu per task is 3.5 per gpus
+#SBATCH --gres=gpu:1
+#SBATCH --mem=32G                        # memory per node
+#SBATCH --time=02-00:00         # time (DD-HH:MM)
+#SBATCH --account=rrg-lplevass
+#SBATCH --job-name=TrainHST_i
+#SBATCH --output=%x-%j.out
+
+source $HOME/environments/caustic/bin/activate
+python $HOME/scratch/hst_diffusion_project/train_score.py\
+    --dataset_path=$HOME/scratch/hst_diffusion_project/data/\
+    --channels 2\
+    --parameter_path=$HOME/scratch/hst_diffusion_project/architectures/unconditional_ncsnnpp.json\
+    --checkpoints_directory=$HOME/scratch/hst_diffusion_project/ncsnpp_hst_i_unconditional\
+    --batch_size=4\
+    --epochs=10000\
+    --max_time=47\
+
--- train_score.py
+++ train_score.py
@@ -7,10 +7,10 @@ import h5py
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class Dataset(torch.utils.data.Dataset):
-    def __init__(self, path, channel=2, condition_on_z=False, condition_on_sed=False, condition_on_mag=False):
+    def __init__(self, path, channels=2, condition_on_z=False, condition_on_sed=False, condition_on_mag=False):
         self.file = h5py.File(path)
         self.len = len(self.file["hudf_resized"])
-        self.channel = channel
+        self.channels = channel
         self.condition_on_z = condition_on_z
         self.condition_on_mag = condition_on_mag
         self.condition_on_sed = condition_on_sed
@@ -19,16 +19,15 @@ class Dataset(torch.utils.data.Dataset):
         return self.len
     
     def __getitem__(self, index):
-        image = torch.tensor(self.file["hudf_resized"][index, self.channel].astype(np.float32)).to(DEVICE)[None]
+        image = torch.tensor(self.file["hudf_resized"][index, self.channel].astype(np.float32)).to(DEVICE)
+        if image.ndim == 2:
+            image = image[None]
         args = []
         if self.conditioned_on_z:
-            z = torch.tensor(self.file["hudf_z"][index, self.channel].astype(np.float32)).to(DEVICE)[None]
+            z = torch.tensor(self.file["hudf_z"][index].astype(np.float32)).to(DEVICE)
             args.append(z)
-        if self.condition_on_mag:
-            mag = torch.tensor(self.file["hudf_mags"][index, self.channel].astype(np.float32)).to(DEVICE)[None]
-            args.append(mag)
         if self.condition_on_sed
-            sed = torch.tensor(self.file["hudf_ids"][index, self.channel].astype(np.float32)).to(DEVICE)[None]
+            sed = torch.tensor(self.file["hudf_template"][index, self.channel].astype(np.float32)).to(DEVICE)
             args.append(sed)
         return image, *args
             
@@ -47,14 +46,14 @@ def main(args):
     
     net = NCSNpp(**hp)
     model = ScoreModel(net, **hp)
-    dataset = Dataset(args.dataset_path, args.channel)
+    dataset = Dataset(args.dataset_path, args.channels)
     model.fit(
             dataset,
             preprocessing_fn=preprocessing,
             epochs=args.epochs,
             batch_size=args.batch_size,
-            checkpoint_directory=args.checkpoint_directory
-            learning_rate=args.learnign_rate
+            checkpoints_directory=args.checkpoints_directory
+            learning_rate=args.learning_rate
             )
 
 if __name__ == "__main__":
@@ -64,13 +63,13 @@ if __name__ == "__main__":
     parser.add_argument("--dataset_path", required=True, help="Path to the h5 dataset")
     parser.add_argument("--channels", default=2, nargs="+" type=int, help="Channels to train the model on. Multiple channel can be provided")
     parser.add_argument("--parameter_path", required=True, help="Path to the model json hyperparameter file")
-    parser.add_argument("--checkpoint_directory", required=True, help="Path to the folder where to save the model, created if it does not exist.")
+    parser.add_argument("--checkpoints_directory", required=True, help="Path to the folder where to save the model, created if it does not exist.")
     
     parser.add_argument("--learning_rate", default=1e-4, type=float)
     parser.add_argument("--epochs", default=1000, type=int)
+    parser.add_argument("--max_time", default=np.inf, type=float)
     parser.add_argument("--batch_size", default=16, type=int)
     parser.add_argument("--condition_on_z", action="store_true")
-    parser.add_argument("--condition_on_mags", action="store_true")
     parser.add_argument("--condition_on_sed", action="store_true")
     args = parser.parse_args()
     main()
